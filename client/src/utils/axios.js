de58227e50f0ff9663a0b4f96d7dfa346d42a3dc
Justin Yates <justinyates887@gmail.com>
2023-10-29 17:20:44 -0700

    update URI

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -17,7 +17,7 @@ export const fetchRepositories = async () => {
 
 export const postMailer = async (event) => {
   try {
-    const response = await axios.post(`${config.prodServerURL}/api/send`, event);
+    const response = await axios.post(`${config.prodServerURL}/.netlify/functions/mailer/api/send`, event);
     return response.status;
   } catch (error) {
     throw error;
